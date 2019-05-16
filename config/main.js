112d8a10656125a5c80fde47d436020fe9ad1066
dspint <dsargent@pint.com>
2019-05-15 18:11:00 -0700

    Added `watchReplace` property, which allows the user to replace the default watched files, instead of the default to add them in addition.

--- config/main.js
+++ config/main.js
@@ -1,31 +1,40 @@
 // EXPORT
 // -----------------------------
 module.exports = {
+  // REQUIRED (CORE)
   // Convert xxxx.html files to xxxx/index.html
   // NOTE: If set to `false`, you must add the .html extension in <a href="xxxx.html"> paths
   convertPageToDirectory: true,
   // The name of the compiled, 'public' directory (`dist`, `public`, etc.)
   distPath: 'dist',
+  // The path to the dev source files. This is used for running the local dev livereload server.
+  srcPath: 'src',
+  // The file to load in `srcPath` if not `index.html`
+  startPath: 'index.html',
+
+  // REQUIRED (SITE)
+  // Add config items here for your own custom build plugins
+
+  // OPTIONAL
   // Add regex patterns to exclude files from being modified once copied to the /dist directory
   // For example, /dist\/vendor will exclude files in /dist/vendor 
   // Can be single regexes: /dist\/path/ or new RegExp(/dist\path/)
   // Or regexes in an array: [/dist\/path/, new RegExp(/dist\path/)]
   //excludePaths: [/dist\/vendors/],
+
   // Set custom 'active' state on links whose [href] matches the current window location path (link active state - defaults as `data-active`)
   // activeAttr: 'zc-active',
+
   // Change the default [attribute] for includes and inline link/scripts
   //includeAttr: 'include',
   //inlineAttr: 'inline',
-  // By default, external links that start with `www` or `cdn` and don't have a protocol manually set (`http://` or `https://`)
-  // have `http://` added so the build doesn't think they are local links. To skip, or add to, define your own value array.
-  // replaceExternalLinkProtocol: { enabled: true, match: ['cdn'] },
-  // The path to the dev source files. This is used for running the local dev livereload server.
-  srcPath: 'src',
-  // The file to load in `srcPath` if not `index.html`
-  startPath: 'index.html',
+
   // Live reload dev browser when these files change (`/assets/css`,`/assets/plugin` (.css and .js), and all `.html` by default)
-  // Note: Adding paths here will not 'add' to the defaults, but will instead override them
-  //watch: [ '/css/*.css', ],
+  // Note: Added paths here are *in addition* to the defaults. To replace the defaults, set the `watchReplace` option listed below
+  //watch: [ '/docs/css/*.css', ],
+  // Set this option to only watch the paths listed in this config file and not the default paths
+  //watchReplace: true,
+
   // Add per-site plugins to use during build process
   // plugins: ['testLogger'],
   // customData: 'getWordpressData',
